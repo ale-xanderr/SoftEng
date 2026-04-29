f75be7b2bce7d6fdcf2b3205f8855534ea762581
ale-xanderr <seaquino@my.cspc.edu.ph>
2026-04-29 16:43:08 +0800

    Intentionally breaking the test_app.py

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_hello():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, CI/CD World!"
+    assert response.data == b"Hello, Broken World!"
