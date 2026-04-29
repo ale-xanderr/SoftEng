7bd60200b1972dabd3e59f741479d83a5b58cef6
ale-xanderr <seaquino@my.cspc.edu.ph>
2026-04-29 16:22:07 +0800

    Start

--- test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_hello():
-    # We use Flask's test_client to simulate a request without running the server
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert response.data == b"Hello, CI/CD World!"  
