9b61fea57e4169bbb3ea5df207c0aeea42afd87e
ale-xanderr <seaquino@my.cspc.edu.ph>
2026-04-29 15:44:38 +0800

    Intentioanlly breaking the test-app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_hello():
     # We use Flask's test_client to simulate a request without running the server
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, CI/CD World!"
+    assert response.data == b"Hello, Broken World!"
